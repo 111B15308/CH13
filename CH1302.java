1e713a84b740b13f3471d51a32cc3c905fd5e5e6
111B15308 <145731748+111B15308@users.noreply.github.com>
2023-12-13 23:42:17 +0800

    Create CH1302.java

---
+++ CH1302.java
@@ -0,0 +1,14 @@
+public class CH1302 {
+    public static void main(String[] args) {
+        int num = 12, den = 0;
+
+        if (den != 0) {
+            int ans = num / den;
+            System.out.println("Result of division: " + ans);
+        } else {
+            System.out.println("除數為 0");
+        }
+
+        System.out.println("End of main()!!");
+    }
+}
